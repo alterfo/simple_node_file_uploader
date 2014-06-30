4c625425de62062dc02b7f96574e288ecd4daccd
unknown <kuzmino@RUDSCU532831.russia.local>
2014-06-30 19:00:36 +0400

    init commit

---
+++ index.js
@@ -0,0 +1,34 @@
+var fs = require('fs'),
+    http = require('http'),
+    connect = require('connect');
+
+var upload_folder = __dirname + "/uploads/"
+
+var app = connect()
+    .use(connect.logger('dev'))
+    .use(connect.static('app'))
+    .use(connect.json())
+    .use(connect.urlencoded())
+    .use(function(req, res) {
+        console.log(req.headers.x_filename);
+        if (req.method.toUpperCase() == "POST" && req.url == "/upload") {
+            var newFile = fs.createWriteStream(upload_folder + req.headers.x_filename);
+            var fileBytes = req.headers['content-length'];
+            var uploadedBytes = 0;
+
+            req.pipe(newFile);
+
+            req.on('data', function(chunk) {
+                uploadedBytes += chunk.length;
+                var progress = (uploadedBytes / fileBytes) * 100;
+                console.log("progress: " + parseInt(progress) + "%");
+            });
+            req.on("end", function() {
+                res.writeHead(200);
+                res.end("done!");
+            })
+        }
+    });
+http.createServer(app).listen(8080, function() {
+    console.log("listening on 8080");
+});
